f17ca4e65620d68ed2f48d773ce7d03d9b6b1d6c
mitul200 <mitul5267@gmail.com>
2024-07-31 14:02:55 +0600

    added work file

--- src/component/Work.jsx
+++ src/component/Work.jsx
@@ -1,8 +1,105 @@
+import img1 from "../assets/portfolio-img1.png";
+import img2 from "../assets/portfolio-img2.png";
+import img3 from "../assets/portfolio-img3.png";
+import { motion } from "framer-motion";
+import { fadeIn } from "./variants";
 const Work = () => {
   return (
-    <div className="section" id="work">
-      work
-    </div>
+    <section className="section" id="work">
+      <div className="container mx-auto">
+        <div className="flex flex-col lg:flex-row gap-x-10">
+          {/* <div> */}
+          <motion.div
+            variants={fadeIn("right", 0.2)}
+            initial="hidden"
+            whileInView={"show"}
+            viewport={{ once: false, amount: 0.7 }}
+            className="flex-1 flex flex-col gap-y-[105px] mb-10 lg:mb-0"
+          >
+            {/* text  */}
+            <div>
+              <h2 className="h2 leading-tight text-accent">
+                my Latest <br />
+                Work
+              </h2>
+              <p className="max-w-sm mb-2">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore
+                eligendi, quisquam perspiciatis totam ut nemo dolor ullam odio
+                alias quis?
+              </p>
+              <button className="btn btn-sm">View all projects</button>
+            </div>
+            {/* img  */}
+            <div className="group relative overflow-hidden border-2 border-white/50  rounded-xl ">
+              {/* overlay  */}
+              <div className=" group-hover:bg-black/70 w-full h-full absolute z-40 transition-all duration-300"></div>
+              {/* img */}
+              <img
+                className=" group-hover:scale-125 transition-all duration-500"
+                src={img1}
+                alt=""
+              />
+              {/* practice   */}
+              <div className="absolute bottom-full left-12 group-hover:bottom-24 transition-all duration-500 z-50">
+                UI/UX Desing{" "}
+              </div>
+              {/* title */}
+              <div className="absolute bottom-full left-12 group-hover:bottom-14 transition-all duration-700 z-50">
+                <span>projects title</span>
+              </div>
+            </div>
+          </motion.div>
+
+          <motion.div
+            variants={fadeIn("left", 0.2)}
+            initial="hidden"
+            whileInView={"show"}
+            viewport={{ once: false, amount: 0.7 }}
+            className=" flex-1 flex flex-col gap-y-10 "
+          >
+            <div className="group relative overflow-hidden border-2 border-white/50  rounded-xl ">
+              {/* overlay  */}
+              <div className=" group-hover:bg-black/70 w-full h-full absolute z-40 transition-all duration-300"></div>
+              {/* img */}
+              <img
+                className=" group-hover:scale-125 transition-all duration-500"
+                src={img1}
+                alt=""
+              />
+              {/* practice   */}
+              <div className="absolute bottom-full left-12 group-hover:bottom-24 transition-all duration-500 z-50">
+                UI/UX Desing{" "}
+              </div>
+              {/* title */}
+              <div className="absolute bottom-full left-12 group-hover:bottom-14 transition-all duration-700 z-50">
+                <span>projects title</span>
+              </div>
+            </div>
+            {/* img 3  */}
+            <div className="group relative overflow-hidden border-2 border-white/50  rounded-xl ">
+              {/* overlay  */}
+              <div className=" group-hover:bg-black/70 w-full h-full absolute z-40 transition-all duration-300"></div>
+              {/* img */}
+              <img
+                className=" group-hover:scale-125 transition-all duration-500"
+                src={img1}
+                alt=""
+              />
+              {/* practice   */}
+              <div className="absolute bottom-full left-12 group-hover:bottom-24 transition-all duration-500 z-50">
+                UI/UX Desing{" "}
+              </div>
+              {/* title */}
+              <div className="absolute bottom-full left-12 group-hover:bottom-14 transition-all duration-700 z-50">
+                <span>projects title</span>
+              </div>
+            </div>
+          </motion.div>
+
+          {/* </div> */}
+        </div>
+      </div>
+    </section>
   );
 };
 
